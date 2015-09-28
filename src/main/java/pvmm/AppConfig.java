1ae51eb5ae22b9b38f8185c3b2a0c91b64411f24
Carlos A. Gomes <carlos.algms@gmail.com>
2015-09-27 23:28:42 -0300

    iniciada configuração para views

---
+++ src/main/java/pvmm/AppConfig.java
@@ -0,0 +1,44 @@
+package pvmm;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "pvmm.*" })
+@Import({ SecurityConfig.class })
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    public void AppConfig() {
+        System.out.println("rodou ?????????????????????????");
+    }
+
+    @Bean(name = "dataSource")
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/pvmm_db");
+        driverManagerDataSource.setUsername("root");
+        driverManagerDataSource.setPassword("123456789");
+        return driverManagerDataSource;
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/webapp/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
---
+++ src/main/java/pvmm/SecurityConfig.java
@@ -0,0 +1,47 @@
+package pvmm;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery( "SELECT username, password, enabled FROM users WHERE username=?")
+                .authoritiesByUsernameQuery( "SELECT username, role from user_roles WHERE username=?" );
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .failureUrl("/login?error")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .and()
+                .logout()
+                    .logoutSuccessUrl("/login?logout")
+                .and()
+                .exceptionHandling()
+                    .accessDeniedPage("/403")
+                .and()
+                .csrf();
+    }
+}
